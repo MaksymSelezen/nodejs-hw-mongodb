be1b2c3e0fdfcf16a31466fbbc19ca1470ee13a5
MaksymSelezen <foqney@gmail.com>
2025-03-15 17:24:47 +0200

    hw 3

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -9,11 +9,11 @@ export const initMongoConnection = async () => {
     const db = getEnvVar('MONGODB_DB');
 
     await mongoose.connect(
-      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=Contacts`,
+      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=Cluster0`,
     );
     console.log('Mongo connection successfully established!');
-  } catch (e) {
-    console.log('Error while setting up mongo connection', e);
-    throw e;
+  } catch (err) {
+    console.log('Error while setting up mongo connection', err);
+    throw err;
   }
 };
