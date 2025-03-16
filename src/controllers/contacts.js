3c2c4c2ce3d726a589216d7c929f106a9dc54e5d
MaksymSelezen <foqney@gmail.com>
2025-03-16 14:21:29 +0200

    fix err

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -48,7 +48,7 @@ export const updateContactController = async (req, res, next) => {
   const result = await updateContact(contactId, req.body);
 
   if (!result) {
-    return next(createHttpError(404, 'Contact not found'));
+    throw createHttpError(404, 'Contact not found');
   }
 
   res.status(200).json({
@@ -63,7 +63,7 @@ export const deleteContactController = async (req, res, next) => {
   const contact = await deleteContact(contactId);
 
   if (!contact) {
-    return next(createHttpError(404, 'Contact not found'));
+    throw createHttpError(404, 'Contact not found');
   }
 
   res.status(204).send();
